8847e88226c39c29b917a2c5b5172e9de42da399
Mich S <sajorda.mich@gmail.com>
2021-05-01 20:38:08 +0800

    add the angular connect to dotnet

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client'; 
+  users: any;
+
+  constructor(private http: HttpClient)
+  {
+
+  }
+
+  ngOnInit() {  
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/user/').subscribe(response =>
+    {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+}
